50c71178071e62b304749063c3bde1d0ac7143fc
JaeUpSu <hyeongs2323@gmail.com>
2022-12-02 20:22:00 +0900

    feat(Problem1): 페이지 게임의 예외처리

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,23 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        try {
+            validatePages(pobi);
+            validatePages(crong);
+
+        //    return getPageGameResult(pobi, crong);
+        } catch (IllegalArgumentException e) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private static void validatePages(List<Integer> player) {
+        int difference = player.get(1) - player.get(0);
+
+        if (difference != 1) {
+            throw new IllegalArgumentException();
+        }
     }
 }
