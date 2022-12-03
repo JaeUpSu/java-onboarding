c80fd28cd784af57fb2666fa0e5e890a09d9d73c
JaeUpSu <hyeongs2323@gmail.com>
2022-12-03 19:36:38 +0900

    feat(Problem3): 해당 숫자의 각 자릿수에 3,6,9 포함 개수 세는 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 public class Problem3 {
+    private static final Character THREE = '3';
+    private static final Character SIX = '6';
+    private static final Character NINE = '9';
+
     public static int solution(int number) {
         int answer = play369Game(number);
         return answer;
@@ -9,13 +13,25 @@ public class Problem3 {
     private static int play369Game(int number) {
         int count = 0;
         for (int i = 1; i <= number; i++) {
-            String numberWords = String.valueOf(number);
-            count += getClapsCount(numberWords);
+            String numberWords = String.valueOf(i);
+            if (is369Contains(numberWords)) {
+                count += getClapsCount(numberWords);
+            }
         }
         return count;
     }
 
-    private static int getClapsCount(String numberWords) {
-        return 0;
+    private static long getClapsCount(String numberWords) {
+        return numberWords.chars()
+                .filter(c -> isDivisible(c, 3))
+                .count();
+    }
+    private static boolean isDivisible(int number, int byNumber) {
+        return ((number - '0') % byNumber == 0) && (number - '0') > 0;
+    }
+    private static boolean is369Contains(String number) {
+        return number.contains(THREE.toString())
+                || number.contains(SIX.toString())
+                || number.contains(NINE.toString());
     }
 }
