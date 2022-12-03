1a466d4a4c525692f6e5afa086a183fb401e0e1b
JaeUpSu <hyeongs2323@gmail.com>
2022-12-03 18:43:31 +0900

    feat(Problem3): 입력받은 숫자 까지 게임 실행 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        int answer = play369Game(number);
         return answer;
+    }
+
+    private static int play369Game(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            String numberWords = String.valueOf(number);
+            count += getClapsCount(numberWords);
+        }
+        return count;
+    }
+
+    private static int getClapsCount(String numberWords) {
+        return 0;
     }
 }
