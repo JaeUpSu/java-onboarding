bc0f99dbc0b7ad80d61e80898a9150c2a1051229
JaeUpSu <hyeongs2323@gmail.com>
2022-12-03 20:10:56 +0900

    refactor(Problem4): 대문자 확인 기능 메서드 제거

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -24,7 +24,7 @@ public class Problem4 {
         if (alpha.equals(BLANK)) {
             return String.valueOf(alpha);
         }
-        if (isUpperAlpha(alpha)) {
+        if (Character.isUpperCase(alpha)) {
             return getReverse(alpha, UPPER_START_ALPHA, UPPER_END_ALPHA);
         }
 
@@ -34,11 +34,5 @@ public class Problem4 {
     private static String getReverse(Character alpha, Character start, Character end) {
         int difference = alpha - start;
         return String.valueOf((char) (end - difference));
-    }
-    private static boolean isUpperAlpha(Character alpha) {
-        if (UPPER_START_ALPHA <= alpha && UPPER_END_ALPHA >= alpha) {
-            return true;
-        }
-        return false;
     }
 }
