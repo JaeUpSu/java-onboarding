52019ba29cf7f369bd5cb744f635c14a55b1e880
JaeUpSu <hyeongs2323@gmail.com>
2022-12-03 18:31:41 +0900

    feat(Problem2): 중복문자 리스트 반환 메서드와 삭제 메서드 정의

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,10 @@
 package onboarding;
 
 
+import java.io.FilterOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = decoding(cryptogram);
@@ -9,18 +13,48 @@ public class Problem2 {
 
     private static String decoding(String cryptogram) {
         while (true) {
-            String nextCryptogram = deleteDuplicate(cryptogram);
-            if (isChange(nextCryptogram, cryptogram)) {
+            List<String> storage = getDuplicateStorage(cryptogram);
+            String nextCryptogram = getDecoding(storage, cryptogram);
+            if (!isChange(nextCryptogram, cryptogram)) {
                 break;
             }
             cryptogram = nextCryptogram;
+            if (cryptogram.isEmpty()) {
+                break;
+            }
         }
         String result = cryptogram;
         return result;
     }
 
-    private static String deleteDuplicate(String cryptogram) {
-        return "";
+    private static String getDecoding(List<String> storage, String cryptogram) {
+        for (String words : storage) {
+            cryptogram = cryptogram.replace(words, "");
+        }
+        return cryptogram;
+    }
+
+    private static List<String> getDuplicateStorage(String cryptogram) {
+        List<String> storage = new ArrayList<>();
+        String words = String.valueOf(cryptogram.charAt(0));
+        for (int i = 1; i < cryptogram.length(); i++) {
+            String currentWord = String.valueOf(cryptogram.charAt(i));
+            if (words.contains(currentWord)) {
+                words += currentWord;
+                continue;
+            }
+            storage = addWords(storage, words);
+            words = currentWord;
+        }
+        storage = addWords(storage, words);
+        return storage;
+    }
+
+    private static List<String> addWords(List<String> storage, String words) {
+        if (words.length() > 1) {
+            storage.add(words);
+        }
+        return storage;
     }
 
     private static boolean isChange(String nextCryptogram, String cryptogram) {
