4522353ed9a912b06f9fd57c73ea4ab74ad51228
JaeUpSu <hyeongs2323@gmail.com>
2022-12-03 19:43:47 +0900

    feat(Problem4): 입력값의 각 자리의 알파벳을 반복하여 뒤집기 실행하는 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,20 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
+        String answer = getReverseGame(word);
         return answer;
+    }
+
+    private static String getReverseGame(String word) {
+        String result = "";
+        for (int i = 0; i < word.length(); i++) {
+            char alpha = word.charAt(i);
+            result += getReverseAlpha(alpha);
+        }
+        return result;
+    }
+
+    private static String getReverseAlpha(char alpha) {
+        return "";
     }
 }
