38485834786104f7ad2a40f0b4493eab2a40d099
JaeUpSu <hyeongs2323@gmail.com>
2022-12-03 20:01:15 +0900

    feat(Problem4): 띄어쓰기 인지 확인 기능 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,6 +1,11 @@
 package onboarding;
 
 public class Problem4 {
+    private static final Character BLANK = ' ';
+    private static final Character UPPER_START_ALPHA = 'A';
+    private static final Character UPPER_END_ALPHA = 'Z';
+    private static final Character LOWER_START_ALPHA = 'a';
+    private static final Character LOWER_END_ALPHA = 'z';
     public static String solution(String word) {
         String answer = getReverseGame(word);
         return answer;
@@ -9,13 +14,19 @@ public class Problem4 {
     private static String getReverseGame(String word) {
         String result = "";
         for (int i = 0; i < word.length(); i++) {
-            char alpha = word.charAt(i);
+            Character alpha = word.charAt(i);
             result += getReverseAlpha(alpha);
         }
         return result;
     }
 
-    private static String getReverseAlpha(char alpha) {
+    private static String getReverseAlpha(Character alpha) {
+        if (alpha.equals(BLANK)) {
+            return String.valueOf(alpha);
+        }
+
+        // getReverse(alpha)
+
         return "";
     }
 }
