300cce3b3d1e136b7d3ceab9dfa81a7c4449c075
JaeUpSu <hyeongs2323@gmail.com>
2022-12-03 17:15:18 +0900

    feat(Problem2): 해독 메서드 구조 정의

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,6 +8,25 @@ public class Problem2 {
     }
 
     private static String decoding(String cryptogram) {
+        while (true) {
+            String nextCryptogram = deleteDuplicate(cryptogram);
+            if (isChange(nextCryptogram, cryptogram)) {
+                break;
+            }
+            cryptogram = nextCryptogram;
+        }
+        String result = cryptogram;
+        return result;
+    }
+
+    private static String deleteDuplicate(String cryptogram) {
         return "";
+    }
+
+    private static boolean isChange(String nextCryptogram, String cryptogram) {
+        if (nextCryptogram.equals(cryptogram)) {
+            return false;
+        }
+        return true;
     }
 }
